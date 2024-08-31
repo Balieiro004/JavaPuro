e98cd66490c4a1a906be4370b0e0ee6d7b3103d7
balieiro004 <balieiro004@gmail.com>
2024-08-31 17:13:26 -0300

    Dividindo em classes o programa, criando a classe Aluno

---
+++ Programa/escola/Aluno.java
@@ -0,0 +1,49 @@
+package escola;
+
+public class Aluno {
+    private String nome;
+    private float[] notas;
+    private float media;
+
+    Aluno(String nome){
+        this.nome = nome;
+        this.notas = new float[3];
+    }
+    //
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    //
+    public float[] getNota(){
+        return notas;
+    }
+    //
+    public void setNota(float[] notas){
+        this.notas = notas;
+        calcularMedia();
+    }
+    //
+    public float getMedia(){
+        return media;
+    }
+
+    private void calcularMedia(){
+        float soma = 0;
+        for(float nota: notas){
+            soma += nota;
+        }
+        this.media = soma / notas.length;
+    }
+
+    public void exibirResultado(){
+        if (media >= 6) {
+            System.out.println(String.format("%s, parabéns você passou :), sua média é: %.1f", nome, media));
+        }else{
+            System.out.println(String.format("%s, você está de recuperação :(, sua média é: %.1f", nome, media));
+        }
+    }
+}
