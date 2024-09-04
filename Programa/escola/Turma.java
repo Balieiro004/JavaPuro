50d9803e9b5b58445daf61393fda6544600631d5
balieiro004 <balieiro004@gmail.com>
2024-09-03 22:05:53 -0300

    Ajustando a classe Turma

--- Programa/escola/Turma.java
+++ Programa/escola/Turma.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Turma {
     private char turma;
     private int serie;
-    private List<Aluno> alunos;
+    private List<Pessoa> alunos;
 
     public Turma(char turma, int serie){
         this.turma = turma;
@@ -14,23 +14,13 @@ public class Turma {
         this.alunos = new ArrayList<>();
     }
 
-    public void adicionarAluno(Aluno Aluno){
-        alunos.add(Aluno);
+    public void adicionarAluno(Pessoa aluno){
+        alunos.add(aluno);
     }
 
-    public List<Aluno> getAlunos(){
-        return alunos;
-    }
-    
     public void exibirResultadosFinais(){
         System.out.println("Resultados finais da turma " + turma + " da série " + serie + ":");
-        for(Aluno aluno: alunos){
-            System.out.println("Aluno: " + aluno.getNome());
-            float[] notas = aluno.getNotas();
-            for(int i = 0; i < notas.length; i++){
-                System.out.println(String.format("Nota " + (i + 1) + ": %.1f", notas[i]));
-            }
-            System.out.println("Média: " + aluno.getMedia());
+        for(Pessoa aluno: alunos){
             aluno.exibirInformacoes();
             System.out.println("-----------------------");
         }
