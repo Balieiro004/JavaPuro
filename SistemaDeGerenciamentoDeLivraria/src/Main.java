422cf5f3b22b15510b93e09323914b50bc5d7b9d
balieiro004 <balieiro004@gmail.com>
2024-09-07 13:53:10 -0300

    Novo Execicio para praticar Livraria

---
+++ SistemaDeGerenciamentoDeLivraria/src/Main.java
@@ -0,0 +1,9 @@
+import entity.Livro;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Livro livro = new Livro("Harry Potter", "J.K Rowling", 35.5, 20);
+        
+    }
+}
---
+++ SistemaDeGerenciamentoDeLivraria/src/entity/Livro.java
@@ -0,0 +1,59 @@
+package entity;
+
+public class Livro {
+
+    private String titulo;
+    private String autor;
+    private double preco;
+    private int quantidadeEstoque;
+
+    public Livro(String titulo, String autor, double preco, int quantidadeEstoque) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.preco = preco;
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(int quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public double venderLivro(int quantidade){
+
+        if(quantidade <= this.quantidadeEstoque){
+            this.quantidadeEstoque -= quantidade;
+            return  quantidade * this.preco;
+        }else {
+            System.out.println(String.format("Estoque insuficiente. Apenas %i unidades disponiveis."));
+            return 0;
+        }
+    }
+}
